f74ec2c5afca7d48f64a7320266b985fab53063c
= <=>
2020-09-24 20:59:39 +0500

    heroku port config

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.port || 5000
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
