3a144afbaad54f488a9e39931ea0f0aaae36b081
= <=>
2020-09-24 20:54:20 +0500

    heroku hotfix

--- src/app.js
+++ src/app.js
@@ -17,10 +17,11 @@ app.use('/api/user', require('./routes/user.routes'))
 app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
 
 app.get('*', (req, res) => {
+    console.log(req.url)
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 })
 
-const PORT = config.get('port') || 3000
+const PORT = config.get('port') || 5000
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
